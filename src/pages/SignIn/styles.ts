1f72e6b9497c8f4e9cd5380ae9fa5b0067942411
eek <leonardolp@gmail.com>
2020-08-08 11:24:28 -0300

    Fianlizando autenteicaçao

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signInBackgroundImg from '../../assets/sign-in-background.png';
@@ -14,11 +14,30 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  place-content: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 700px;
+`;
+
+const apperFromLeft = keyframes`
+  from  {
+     opacity: 0;
+     transform: translateX(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  place-content: center;
+
+  animation: ${apperFromLeft} 1s;
 
   form {
     margin: 80px 0;
@@ -62,6 +81,7 @@ export const Content = styled.div`
     }
 
   }
+
 `;
 
 export const Background = styled.div`
